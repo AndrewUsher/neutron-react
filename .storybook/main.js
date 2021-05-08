fa32c7557b4981d38dda1ad01e6b19c9ded54629
Andrew Usher <andrewusher00@gmail.com>
2021-05-08 17:47:13 -0500

    chore: update deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@
 const path = require('path')
 
 module.exports = {
-  stories: ['../stories/**/*.stories.(js|mdx)'],
+  stories: ['../stories/**/*.stories.@(js|mdx)'],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-links',
