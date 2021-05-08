560f0118589982934f1985b24d6e70289100277b
Andrew Usher <andrewusher00@gmail.com>
2021-05-08 17:43:13 -0500

    refactor: upgrade to storybook 6

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,15 +4,6 @@ const path = require('path')
 module.exports = {
   stories: ['../stories/**/*.stories.(js|mdx)'],
   addons: [
-    {
-      name: '@storybook/preset-typescript',
-      options: {
-        include: [
-          path.resolve(__dirname, '../src'),
-          path.resolve(__dirname, '../stories')
-        ]
-      }
-    },
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-docs',
