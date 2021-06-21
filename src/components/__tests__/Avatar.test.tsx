301ea4e9d7a416c5540d427ca53e7707695c1947
Andrew Usher <andrew.usher@autozone.com>
2021-06-21 17:49:20 -0500

    chore: update tests for Avatar

--- src/components/__tests__/Avatar.test.tsx
+++ src/components/__tests__/Avatar.test.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { Avatar, ThemeProvider } from '../../'
 
 describe('Avatar', () => {
   test('renders image', () => {
-    const { getByAltText } = render(
+    render(
       <ThemeProvider>
         <Avatar src="http://placekitten.com/200/300" alt="test" />
       </ThemeProvider>
     )
-    expect(getByAltText('test')).toBeInTheDocument()
+    expect(screen.getByAltText('test')).toBeInTheDocument()
+  })
+
+  test('render text if src is not defined', () => {
+    render(
+      <ThemeProvider>
+        <Avatar text="AU" />
+      </ThemeProvider>
+    )
+    expect(screen.getByText('AU')).toBeInTheDocument()
   })
 })
